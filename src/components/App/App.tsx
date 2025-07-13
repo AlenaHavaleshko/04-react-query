a6bc7ee94a364e9562370f8fb5d23d495bc6aed4
Alena Havalehko <126282451+AlenaHavaleshko@users.noreply.github.com>
2025-07-13 10:58:19 +0300

    refactoring

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,8 @@
 import { useState } from "react";
 import css from "./App.module.css";
-import { useQuery } from "@tanstack/react-query";
-import ReactPaginate from 'react-paginate';
-import { Toaster 
-
-
- } from "react-hot-toast";
+import { useQuery, keepPreviousData } from "@tanstack/react-query";
+import ReactPaginate from "react-paginate";
+import { Toaster } from "react-hot-toast";
 import type { Movie } from "../../types/movie";
 import { fetchMovies } from "../../services/movieService";
 import SearchBar from "../SearchBar/SearchBar";
@@ -15,26 +12,24 @@ import ErrorMessage from "../ErrorMessage/ErrorMessage";
 import MovieModal from "../MovieModal/MovieModal";
 
 export default function App() {
-
   const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
-  const [searchValue, setSearchValue] = useState('');
+  const [searchValue, setSearchValue] = useState("");
   const [page, setPage] = useState(1);
 
-  const { data, error, isLoading, isError } = useQuery({
-    queryKey: ['movies', searchValue, page],
-    queryFn: () => fetchMovies( { query: searchValue, page }),
+  const { data, error, isLoading, isError, isSuccess } = useQuery({
+    queryKey: ["movies", searchValue, page],
+    queryFn: () => fetchMovies({ query: searchValue, page }),
     enabled: searchValue.length > 0, //  Запит активується тільки після введення пошукового запитуесть запрос
+    placeholderData: keepPreviousData,
   });
 
-  //console.log(data);
-
   const handleSubmit = (newSearchValue: string) => {
-  setSearchValue(newSearchValue); // передаётся в useQuery
-  setPage(1); // сбрасываем пагинацию на первую страницу
-};
+    setSearchValue(newSearchValue); // передаётся в useQuery
+    setPage(1); // сбрасываем пагинацию на первую страницу
+  };
 
-const handlePageClick = ({ selected }: { selected: number }) => {
-    setPage(selected + 1); // Змінюємо сторінку на вибрану
+  const handlePageClick = ({ selected }: { selected: number }) => {
+    setPage(selected + 1); // Змінюємо сторінку на вибрану в реакт пагинейт
   };
 
   const handleSelect = (movie: Movie) => {
@@ -54,7 +49,11 @@ const handlePageClick = ({ selected }: { selected: number }) => {
       <SearchBar onSubmit={handleSubmit} />
 
       {isLoading && <Loader />}
-      {isError && <ErrorMessage message={error instanceof Error ? error.message : "Unknown error"} />}
+      {isError && (
+        <ErrorMessage
+          message={error instanceof Error ? error.message : "Unknown error"}
+        />
+      )}
 
       {data?.results?.length === 0 && (
         <p>No movies found. Try a different search query.</p>
@@ -62,22 +61,24 @@ const handlePageClick = ({ selected }: { selected: number }) => {
 
       {/* Рендеримо список фільмів, якщо є дані */}
       {data?.results && (
-      <MovieGrid movies={data.results} onSelect={handleSelect} />
+        <MovieGrid movies={data.results} onSelect={handleSelect} />
       )}
 
-       {/* Пагінація */} 
-      {totalPages > 1 && (
+      {/* Пагінація */}
+      {isSuccess && totalPages > 1 && (
         <ReactPaginate
           pageCount={totalPages}
           pageRangeDisplayed={5}
           marginPagesDisplayed={1}
           onPageChange={handlePageClick}
           forcePage={page - 1}
+          renderOnZeroPageCount={null}  // не рендерити ,якщо pageCount = 0
           containerClassName={css.pagination}
           activeClassName={css.active}
           nextLabel="→"
           previousLabel="←"
-         />
+          breakLabel="..."
+        />
       )}
 
       {selectedMovie && (
